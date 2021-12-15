eeae390b2d54e1215e1c7d047bd61b4fd374754d
scott <slimzel@protonmail.com>
2021-12-15 09:15:11 -0800

    removed apollo

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import { ApolloProvider } from "@apollo/client";
 import Router from "next/router";
 import Page from "@/components/Page";
 import NProgress from "nprogress";
@@ -6,7 +5,6 @@ import "@/styles/globals.css";
 import "@/styles/nprogress.css";
 
 import { CartStateProvider } from "@/lib/cartState";
-import client from "../apollo-client";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -15,13 +13,11 @@ Router.events.on("routeChangeError", () => NProgress.done());
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <ApolloProvider client={client}>
-        <CartStateProvider>
-          <Page>
-            <Component {...pageProps} />
-          </Page>
-        </CartStateProvider>
-      </ApolloProvider>
+      <CartStateProvider>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </CartStateProvider>
     </>
   );
 }
