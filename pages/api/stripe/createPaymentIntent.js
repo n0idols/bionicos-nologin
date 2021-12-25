548514cda99be57623e514609183f316fbb6aa91
scott <slimzel@protonmail.com>
2021-12-24 17:16:46 -0800

    fixing server bug

--- pages/api/stripe/createPaymentIntent.js
+++ pages/api/stripe/createPaymentIntent.js
@@ -1,5 +1,3 @@
-import { supabase } from "@/lib/supabaseClient";
-
 const stripe = require("stripe")(
   "sk_test_51JxjYrJpULzH3yu6Vy486PK87ceDaxQa7bfXUwSP0UiCfbpB8XFnAmpXziOx6U1HcRnQPfwQCHPgSTXoUoXxAsTs00LcEiUEeZ"
 );
@@ -15,32 +13,29 @@ const calculateOrderAmount = (cart) => {
 
   return total;
 };
-const chargeCustomer = async (customerId) => {
-  // Lookup the payment methods available for the customer
-  const paymentMethods = await stripe.paymentMethods.list({
-    customer: customerId,
-    type: "card",
-  });
-  try {
-    // Charge the customer and payment method immediately
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: calculateOrderAmount(cart),
-
-      currency: "usd",
-      customer: customerId,
-      payment_method: paymentMethods.data[0].id,
-      off_session: true,
-      confirm: true,
-    });
-  } catch (err) {
-    // Error code will be authentication_required if authentication is needed
-    console.log("Error code is: ", err.code);
-    const paymentIntentRetrieved = await stripe.paymentIntents.retrieve(
-      err.raw.payment_intent.id
-    );
-    console.log("PI retrieved: ", paymentIntentRetrieved.id);
-  }
-};
+// const chargeCustomer = async (customerId) => {
+//   // Lookup the payment methods available for the customer
+//   const paymentMethods = await stripe.paymentMethods.list({
+//     customer: customerId,
+//     type: "card",
+//   });
+//   try {
+//     // Charge the customer and payment method immediately
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount: 1099,
+//       currency: "eur",
+//       customer: customerId,
+//       payment_method: paymentMethods.data[0].id,
+//       off_session: true,
+//       confirm: true,
+//     });
+//   } catch (err) {
+//     // Error code will be authentication_required if authentication is needed
+//     console.log("Error code is: ", err.code);
+//     const paymentIntentRetrieved = await stripe.paymentIntents.retrieve(err.raw.payment_intent.id);
+//     console.log("PI retrieved: ", paymentIntentRetrieved.id);
+//   }
+// };
 export default async function handler(req, res) {
   const { cart } = req.body;
   try {
--- pages/thankyou.js
+++ pages/thankyou.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Section from "@/components/Section";
 import { supabase } from "@/lib/supabaseClient";
-import { parseCookies, destroyCookie } from "nookies";
+import { parseCookies } from "nookies";
 import { useCart } from "@/lib/cartState";
 export default function ThankYouPage({ user }) {
   const [orderReciept, setOrderReciept] = useState(null);
@@ -32,8 +32,8 @@ export default function ThankYouPage({ user }) {
   return (
     <Section>
       <h1>Thank you for your order!</h1>
-      {/* <pre>{JSON.stringify(orderReciept, null, 2)}</pre> */}
-      <p>A copy of your recipet has been sent to your email </p>
+      <pre>{JSON.stringify(orderReciept, null, 2)}</pre>
+      <p>A copy of your reciept has been sent to your email </p>
       <p>You can also view your orders here</p>
       CONFIRMED: {query.payment_intent}
     </Section>
