f5988f02bd733cf9944aed3d48ff1dad738e8069
scott <slimzel@protonmail.com>
2021-12-27 06:28:28 -0800

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+  // {
+  //   key: "Content-Security-Policy",
+  //   value: "default-src; self'",
+  // },
+];
+
 module.exports = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
