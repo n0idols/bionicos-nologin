50b53f06dc3fde6e2c7a6758b049ae793230d536
scott <slimzel@protonmail.com>
2021-12-27 05:39:18 -0800

    changes

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -1,52 +1,42 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Section from "@/components/Section";
-import { supabase } from "@/lib/supabaseClient";
+
 import parseCookies from "@/lib/cookie";
 import { useCart } from "@/lib/cartState";
-export default function ThankYouPage({ user }) {
+export default function ThankYouPage() {
   const [orderReciept, setOrderReciept] = useState(null);
   const { query } = useRouter();
   const { emptyCart, cart } = useCart;
 
-  useEffect(() => {
-    saveOrder();
-  }, []);
+  // useEffect(() => {
+  //   saveOrder();
+  // }, []);
 
-  async function saveOrder() {
-    const { data, error } = await supabase.from("orders").insert([
-      {
-        user_id: user.id,
-        payment_intent: query.payment_intent,
-        ordered_items: cart,
-      },
-    ]);
-    if (error) {
-      console.log(error);
-      alert(error);
-    } else {
-      setOrderReciept(data);
-    }
-  }
+  // async function saveOrder() {
+  //   const { data, error } = await supabase.from("orders").insert([
+  //     {
+  //       user_id: user.id,
+  //       payment_intent: query.payment_intent,
+  //       ordered_items: cart,
+  //     },
+  //   ]);
+  //   if (error) {
+  //     console.log(error);
+  //     alert(error);
+  //   } else {
+  //     setOrderReciept(data);
+  //   }
+  // }
 
   return (
     <Section>
-      {JSON.stringify(cart)}cart
+      {/* {JSON.stringify(cart)}cart
       <h1>Thank you for your order!</h1>
-      <pre>{JSON.stringify(orderReciept, null, 2)}</pre>
+      <pre>{JSON.stringify(orderReciept, null, 2)}</pre> */}
       <p>A copy of your reciept has been sent to your email </p>
       <p>You can also view your orders here</p>
       CONFIRMED: {query.payment_intent}
     </Section>
   );
-}
 
-export async function getServerSideProps({ req, context }) {
-  const { user } = await supabase.auth.api.getUserByCookie(req);
-  // const cookies = parseCookies(context.req);
-  if (user) {
-    return {
-      props: { user },
-    };
-  }
-}
