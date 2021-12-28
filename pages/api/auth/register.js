bafeea83b073637d5885cd6b00024d2cfaf81183
scott <slimzel@protonmail.com>
2021-12-28 04:28:48 -0800

    named the route

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,7 +1,7 @@
 import cookie from "cookie";
 import { API_URL } from "@/config/index";
 
-export default async (req, res) => {
+const regRoute = async (req, res) => {
   if (req.method === "POST") {
     const { full_name, email, password } = req.body;
 
@@ -43,3 +43,5 @@ export default async (req, res) => {
     res.status(405).json({ message: `Method ${req.method} not allowed` });
   }
 };
+
+export default regRoute;
