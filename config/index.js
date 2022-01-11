e7dcfcd74ffbd0ba6b42d22bc99be3cd75b7af04
scott <slimzel@protonmail.com>
2022-01-10 18:21:04 -0800

    removed unused

--- config/index.js
+++ config/index.js
@@ -1,13 +1,8 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 
-// export const API_URL ="http://localhost:1337";
-
 export const NEXT_URL =
   process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
-
-export const PLACES_ID = process.env.NEXT_GOOGLE_PLACES_ID;
-export const PLACES_KEY = process.env.NEXT_GOOGLE_KEY;
 
 export const REDIRECT_URL =
   process.env.NEXT_REDIRECT_URL || "http://localhost:3000/thankyou";
