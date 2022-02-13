c60b35d14aa2e989f9c9b858ea56a32b8a2dcf28
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-02-13 07:42:24 -0800

    corrected authstate url

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import Hero from "@/components/Hero";
 import Layout from "@/components/Layout";
 
-import Featured from "@/components/Products/Featured";
-
 export default function Home() {
   return (
     <Layout title="Home">
       <Hero />
-      <Featured />
     </Layout>
   );
 }
