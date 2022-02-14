9551f29c6a0ff53ea3df97e49f96e208a2a616ec
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-02-13 17:25:58 -0800

    ok

--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,31 @@ import Layout from "@/components/Layout";
 import MapSection from "@/components/MapSection";
 import Reviews from "@/components/Reviews";
 
-export default function Home({ reviews }) {
+export default function Home({}) {
   return (
     <Layout title="Home">
       <Hero />
-      <Reviews reviews={reviews} />
+      {/* <Reviews reviews={reviews} /> */}
 
       <MapSection />
     </Layout>
   );
-}
+// }
 
-export async function getServerSideProps() {
-  const res = await fetch(
-    `https://api.yelp.com/v3/businesses/bionicos-and-juices-rios-palmdale/reviews`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PRIVATE_YELP_KEY}`,
-      },
-    }
-  );
-  const data = await res.json();
+// export async function getServerSideProps() {
+//   const res = await fetch(
+//     `https://api.yelp.com/v3/businesses/bionicos-and-juices-rios-palmdale/reviews`,
+//     {
+//       headers: {
+//         Authorization: `Bearer ${process.env.NEXT_PRIVATE_YELP_KEY}`,
+//       },
+//     }
+//   );
+//   const data = await res.json();
 
-  return {
-    props: {
-      reviews: data.reviews,
-    },
-  };
-}
+//   return {
+//     props: {
+//       reviews: data.reviews,
+//     },
+//   };
+// }
