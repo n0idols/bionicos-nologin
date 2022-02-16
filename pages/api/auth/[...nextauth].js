84ea42af779cf429e42b2817c212d26cf7829472
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-02-16 08:38:23 -0800

    removed google login temp

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,10 +10,10 @@ import axios from "axios";
 
 const options = {
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
