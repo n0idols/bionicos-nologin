420b44487f52fc95a9458082f242429abf8f9d57
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-03-03 07:12:38 -0800

    ok

--- pages/auth/facebook/callback.js
+++ pages/auth/facebook/callback.js
@@ -1,23 +1,30 @@
 import axios from "axios";
 
 import { withSession } from "@/middlewares/session";
-import { parseCookies } from "@/lib/cookie";
+import { parseCookies } from "nookies";
+import { useEffect } from "react";
+import { useCart } from "@/lib/cartState";
+import { useRouter } from "next/router";
 
 export default function CallbackPage({}) {
+  const router = useRouter();
+  const { cart } = useCart();
+  useEffect(() => {
+    if (cart.length > 0) {
+      router.push("/checkout");
+    } else {
+      router.push("/account/dashboard");
+    }
+  }, []);
   return (
     <div>
       Thank you
-      <p>
-        Youll be redirected to dashboard, if not, click the
-        &quote;account&quote; link.
-      </p>
+      <p>You'll be redirected</p>
     </div>
   );
 }
 
 export const getServerSideProps = withSession(async ({ query, req }) => {
-  let { cart } = parseCookies(req);
-
   try {
     const res = await axios.get(
       `https://bionicos.herokuapp.com/auth/facebook/callback?access_token=${query.access_token}`
@@ -32,20 +39,8 @@ export const getServerSideProps = withSession(async ({ query, req }) => {
   } catch (err) {
     console.log(err);
   }
-  if (cart.length > 0) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/checkout",
-      },
-      props: {},
-    };
-  }
+
   return {
-    redirect: {
-      permanent: false,
-      destination: "/account/dashboard",
-    },
     props: {},
   };
 });
