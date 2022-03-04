9b8ce45372474c5021c75ad6ded9458c590d0ee7
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-03-04 08:31:05 -0800

    ok

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,29 @@ import Hero from "@/components/Hero";
 import Layout from "@/components/Layout";
 import MapSection from "@/components/MapSection";
 import Reviews from "@/components/Reviews";
+import { gql } from "@apollo/client";
+import client from "@/lib/apollo-client";
+import DaySpecial from "@/components/DaySpecial";
+import LeadCapture from "@/components/LeadCapture";
 
-export default function Home({ reviews }) {
+export default function Home({ reviews, specials }) {
+  const d = new Date();
+  const da = d.getDay();
+  const day = da.toString();
   return (
     <Layout title="Home">
       <Hero />
+      {/* {specials.map((special) => {
+        if (special.id === day) return <DaySpecial special={special} />;
+      })} */}
       <Reviews reviews={reviews} />
-
-      <MapSection />
+      {/* <LeadCapture /> */}
+      {/* <MapSection /> */}
     </Layout>
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch(
     `https://api.yelp.com/v3/businesses/bionicos-and-juices-rios-palmdale/reviews`,
     {
@@ -23,11 +33,42 @@ export async function getServerSideProps() {
       },
     }
   );
-  const data = await res.json();
+  const reviewData = await res.json();
+
+  const { data } = await client.query({
+    query: gql`
+      query Specials {
+        specials(sort: "id:asc") {
+          id
+          day
+          product {
+            title
+            description
+            modifiers {
+              name
+              max
+              required
+              mod {
+                name
+                price
+              }
+            }
+          }
+          side
+          price
+          image {
+            url
+            hash
+          }
+        }
+      }
+    `,
+  });
 
   return {
     props: {
-      reviews: data.reviews,
+      reviews: reviewData.reviews,
+      specials: data.specials,
     },
   };
 }
