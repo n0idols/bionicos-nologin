5e1f28310b958b636957fa411a8902881d70b254
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-03-04 08:59:13 -0800

    testing bug fix

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,13 @@ import MapSection from "@/components/MapSection";
 import Reviews from "@/components/Reviews";
 import { gql } from "@apollo/client";
 import client from "@/lib/apollo-client";
-import DaySpecial from "@/components/DaySpecial";
-import LeadCapture from "@/components/LeadCapture";
-import LocationMap from "@/components/LocationMap";
 
 export default function Home({ reviews, specials }) {
-  const d = new Date();
-  const da = d.getDay();
-  const day = da.toString();
   return (
     <Layout title="Home">
       <Hero />
-      {/* {specials.map((special) => {
-        if (special.id === day) return <DaySpecial special={special} />;
-      })} */}
       <Reviews reviews={reviews} />
-      {/* <LeadCapture /> */}
       <MapSection />
-      {/* <LocationMap /> */}
     </Layout>
   );
 }
