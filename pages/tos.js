99539a8c523f1c2c3eccb5d21130b365f36e4160
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-03-10 05:29:39 -0800

    remove unused

--- pages/tos.js
+++ pages/tos.js
@@ -1,16 +1,14 @@
 import client from "@/lib/apollo-client";
 import gql from "graphql-tag";
 import ReactMarkdown from "react-markdown";
-import PleaseSignIn from "../components/PleaseSignIn";
+
 export default function Tos({ tos }) {
   return (
-    <PleaseSignIn>
-      <div className="prose mx-auto pt-4">
-        <article>
-          <ReactMarkdown>{tos}</ReactMarkdown>
-        </article>
-      </div>
-    </PleaseSignIn>
+    <div className="prose mx-auto pt-4">
+      <article>
+        <ReactMarkdown>{tos}</ReactMarkdown>
+      </article>
+    </div>
   );
 }
 
