c3b39fc75d0513c5a70d47cafa938007501a5174
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-03-19 00:04:50 -0700

    reset didng work yet

--- pages/signin.js
+++ pages/signin.js
@@ -1,6 +1,5 @@
 import { useUser } from "@supabase/supabase-auth-helpers/react";
 import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
-import { Auth, Typography, Button } from "@supabase/ui";
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
@@ -54,14 +53,14 @@ export default function SignInPage() {
               <span className="font-semibold underline ml-1">Sign up here</span>
             </a>
           </Link>
-          <Link href="/forgotpassword">
+          {/* <Link href="/forgotpassword">
             <a className="description text-center mt-2 text-sm">
               Forgot Password?
               <span className="font-semibold underline ml-1">
                 Get Help Logging In
               </span>
             </a>
-          </Link>
+          </Link> */}
           {/* <FBLoginBtn /> */}
 
           {/* <div className="mt-8 mb-4">
