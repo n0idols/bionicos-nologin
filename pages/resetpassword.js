ba927a8e43e92d72d61a7533cecefdca4dc163ac
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-03-20 17:11:02 -0700

    typo

--- pages/resetpassword.js
+++ pages/resetpassword.js
@@ -1,4 +1,4 @@
-import { useStatem, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Layout from "@/components/Layout";
