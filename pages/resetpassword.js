14cd2bd2c9b946712ff50c8f61c274718a40ac45
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-03-20 17:09:16 -0700

    ok

--- pages/resetpassword.js
+++ pages/resetpassword.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useStatem, useEffect } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Layout from "@/components/Layout";
