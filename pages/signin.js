46f3a7a898e12e0fadfa13faa91d0822bd2ef8fe
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-04-16 12:19:25 -0700

    signin styles

--- pages/signin.js
+++ pages/signin.js
@@ -1,6 +1,6 @@
 import { useUser } from "@supabase/supabase-auth-helpers/react";
 import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
-
+import { AiOutlineMail } from "react-icons/ai";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
@@ -45,34 +45,50 @@ export default function SignInPage() {
 
   return (
     <Layout title="Sign In">
-      <div className="max-w-md mx-auto mt-24 p-4 rounded-xl bg-white shadow-xl">
-        <h1 className="text-center mt-2">Sign In</h1>
+      <div className="max-w-md mx-auto">
+        <div className="mx-4 mt-24 p-4 rounded-xl bg-white shadow-xl">
+          <h1 className="text-center mt-2">Sign In</h1>
 
-        <GoogleLogin />
-        <form className="form-control" onSubmit={onSubmit} method="post">
-          {/* <FBLoginBtn /> */}
+          <GoogleLogin />
+          <form className="form-control" onSubmit={onSubmit} method="post">
+            {/* <FBLoginBtn /> */}
 
-          <div className="mt-8 mb-4">
-            <h2 className="text-center w-full border-b leading-[.1em] m-[10px 0 20px]">
-              <span className="bg-white px-3">Or</span>
-            </h2>
-          </div>
+            <div className="mt-8 mb-4">
+              <h2 className="text-center w-full border-b leading-[.1em] m-[10px 0 20px]">
+                <span className="bg-white px-3">OR</span>
+              </h2>
+            </div>
 
-          <h2 className="text-center font-light">Use email</h2>
+            <div className="flex justify-center space-x-2 items-center">
+              <p className="font-bold uppercase text-sm">use email</p>
+              <AiOutlineMail />{" "}
+            </div>
+            <Link href="/signup">
+              <a className="description text-center mt-2 text-sm">
+                <span className="font-semibold">Need an account?</span>
+                <span className="underline ml-1">Sign up here</span>
+              </a>
+            </Link>
+            <Link href="/forgotpassword">
+              <a className="description text-center mt-2 text-sm text-primary-focus">
+                <span className="font-semibold"> Forgot Password?</span>
+                <span className=" underline ml-1">Reset your password</span>
+              </a>
+            </Link>
 
-          <label htmlFor="email" className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input
-            className="input input-primary"
-            type="email"
-            placeholder="Your email"
-            autoComplete="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          {/* 
+            <label htmlFor="email" className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              className="input input-primary"
+              type="email"
+              placeholder="Your email"
+              autoComplete="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {/* 
           <label htmlFor="phone" className="label">
             <span className="label-text">Phone</span>
           </label>
@@ -83,43 +99,30 @@ export default function SignInPage() {
             autoComplete="phone"
             name="phone"
           /> */}
-          <label htmlFor="password" className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input
-            className="input input-primary"
-            type="password"
-            placeholder="Password"
-            autoComplete="new-password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {/* <input className="syrrup" type="syrrup" name="syrrup" /> */}
-          <Link href="/signup">
-            <a className="description text-center mt-2 text-sm">
-              Need an account?
-              <span className="font-semibold underline ml-1">Sign up here</span>
-            </a>
-          </Link>
-          <Link href="/forgotpassword">
-            <a className="description text-center mt-2 text-sm text-primary-focus">
-              Forgot Password?
-              <span className="font-semibold underline ml-1">
-                Reset your password
-              </span>
-            </a>
-          </Link>
-          <div className="mt-6">
-            <button
-              type="submit"
-              className="btn btn-block  text-white"
-              disabled={loading}
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
+            <label htmlFor="password" className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              className="input input-primary"
+              type="password"
+              placeholder="Password"
+              autoComplete="new-password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {/* <input className="syrrup" type="syrrup" name="syrrup" /> */}
+            <div className="mt-6">
+              <button
+                type="submit"
+                className="btn btn-block  text-white"
+                disabled={loading}
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </Layout>
   );
