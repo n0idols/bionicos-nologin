4e7a60a663d592f79454a76ec015101c36db7ce8
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-05-20 19:05:30 -0700

    unused

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,41 +1,15 @@
 import React, { MouseEventHandler, useEffect, useState } from "react";
 
-import useSWR from "swr";
 import {
   getUser,
   withAuthRequired,
   supabaseServerClient,
-  supabaseClient,
 } from "@supabase/supabase-auth-helpers/nextjs";
 
 import Profile from "@/components/Profile";
 import Layout from "@/components/Layout";
 
-// const fetcher = async () => {
-//   const response = await fetch("/api/user");
-//   const data = await response.json();
-//   return data;
-// };
-
 const Dashboard = ({ orders, user }) => {
-  // useEffect(() => {
-  //   //fetch customer from supabase customers table
-
-  //   const getSupaCustomer = async () => {
-  //     try {
-  //       const { data, error } = await supabaseClient
-  //         .from("customers")
-  //         .select("stripe_customer")
-  //         .filter("id", "eq", user.id);
-
-  //       setStripeCustomer(data[0].stripe_customer);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   getSupaCustomer();
-  // }, []);
-
   return (
     <Layout title="Profile">
       <div className="flex items-center justify-between">
@@ -48,9 +22,6 @@ const Dashboard = ({ orders, user }) => {
 const getServerSideProps = withAuthRequired({
   redirectTo: "/signin",
   getServerSideProps: async (ctx) => {
-    // const { req } = ctx;
-    // // const { cart } = parseCookies(req);
-
     const { user } = await getUser(ctx);
 
     const { data: orders, error: ordersError } = await supabaseServerClient(ctx)
@@ -58,17 +29,10 @@ const getServerSideProps = withAuthRequired({
       .select("*")
       .order("ordered_at", { ascending: false });
 
-    const { data: stripeCustomer, error: customerError } =
-      await supabaseServerClient(ctx)
-        .from("customers")
-        .select("stripe_customer")
-        .filter("id", "eq", user.id);
-
     return {
       props: {
         user,
         orders,
-        // stripeCustomer,
       },
     };
   },
