36e32cc9f336db22969318eef86a82f49f8f6994
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-05-24 09:51:05 -0700

    static fetch featured items

--- components/FeaturedProduct.js
+++ components/FeaturedProduct.js
@@ -4,38 +4,13 @@ import gql from "graphql-tag";
 import FeaturedItem from "./FeaturedItem";
 import Loading from "./icons/Loading";
 
-export default function FeaturedProduct() {
-  const { loading, error, data } = useQuery(gql`
-    query {
-      products(where: { isFeatured: true }) {
-        id
-        number
-        title
-        description
-        price
-        modifiers(sort: "number:asc") {
-          name
-          required
-          max
-          mod {
-            name
-            price
-          }
-        }
-        image {
-          url
-        }
-      }
-    }
-  `);
-  if (loading) return <Loading />;
-
+export default function FeaturedProduct({ featuredProducts }) {
   return (
     <div className="pt-10 max-w-5xl mx-auto px-2">
       {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
       <h1 className="grad-text">Featured Items</h1>
       <div className="grid  md:grid-cols-3 grid-cols-1 gap-6 ">
-        {data.products.map((item, index) => (
+        {featuredProducts.products.map((item, index) => (
           <FeaturedItem key={index} item={item} />
         ))}
       </div>
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,35 @@ import Hero from "@/components/Hero";
 import Layout from "@/components/Layout";
 import MapSection from "@/components/MapSection";
 import Reviews from "@/components/Reviews";
+import client from "@/lib/apollo-client";
+import { gql, useQuery } from "@apollo/client";
+import axios from "axios";
+import { useState } from "react";
 import FeaturedProduct from "../components/FeaturedProduct";
 
-export default function Home({ reviews }) {
+export default function Home({ reviews, featuredProducts }) {
+  const [order, setOrder] = useState(null);
+  const [response, setResponse] = useState("yo");
+  const handleThat = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/email", {
+        order,
+      });
+
+      setResponse(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Layout title="Home">
+      <button className="btn btn-outline" onClick={handleThat}>
+        sendmail
+      </button>
+      {JSON.stringify(response)}
       <Hero />
-      <FeaturedProduct />
+      <FeaturedProduct featuredProducts={featuredProducts} />
       <Reviews reviews={reviews} />
       {/* <LeadCapture /> */}
       <MapSection />
@@ -27,15 +49,36 @@ export async function getStaticProps() {
   );
   const reviewData = await yelpRes.json();
 
-  // const productRes = await fetch(
-  //   `${process.env.NEXT_PUBLIC_API_URL}/products?isFeatured=true`
-  // );
-  // const productData = await productRes.json();
+  const { data } = await client.query({
+    query: gql`
+      query {
+        products(where: { isFeatured: true }) {
+          id
+          number
+          title
+          description
+          price
+          modifiers(sort: "number:asc") {
+            name
+            required
+            max
+            mod {
+              name
+              price
+            }
+          }
+          image {
+            url
+          }
+        }
+      }
+    `,
+  });
 
   return {
     props: {
       reviews: reviewData.reviews,
-      // products: productData,
+      featuredProducts: data,
     },
   };
 }
