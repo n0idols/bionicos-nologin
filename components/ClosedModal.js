fcfca0fdc4ab09110ea86f06d9edf7c135583248
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-05-31 05:49:07 -0700

    regular hours

--- components/ClosedModal.js
+++ components/ClosedModal.js
@@ -11,28 +11,22 @@ export default function ClosedModal() {
     date.setMilliseconds(date.getUTCMilliseconds());
 
     // if (date.getDay() === 0) return true;
-    return date.getHours() < 6 || date.getHours() >= 20;
+    return date.getHours() < 6 || date.getHours() >= 15;
   };
 
   return (
     <>
-      <Modal title="⚠️ We are currently closed" show="true">
+      <Modal title="⚠️ We are currently closed" show={isClosed()}>
         <div className="flex px-4">
           <div className="flex flex-col justify-center items-center ">
             <p>😔</p>
-            {/* <p className="italic text-center mb-2 text-sm">
-              Please come back when we open!
-            </p> */}
             <p className="italic text-center mb-2 text-sm">
-              We are closed for Memorial Day
+              Please come back when we open!
             </p>
-            <h2 className="text-center">
-              We will reopen Tuesday, May 31 @ 7:00am{" "}
-            </h2>
 
-            {/* <h2>
+            <h2>
               MONDAY - SUNDAY: <span className="block ">7:00am - 4:00pm</span>{" "}
-            </h2> */}
+            </h2>
           </div>
           <div className="w-1/2 px-4">
             <ClosedIcon />
