9c4b3cdcd36eee897b65532b5e237a138f784cc1
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-06-01 18:05:20 -0700

    removed admin pages

--- pages/account/admin/customers/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function CustomerIndex() {
-  return <div>CustomerIndex</div>;
-}
--- pages/contact/index.js
+++
@@ -1,9 +0,0 @@
-import Section from "@/components/Section";
-
-export default function ContactIndex() {
-  return (
-    <Section>
-      <h1>Contact Us</h1>
-    </Section>
-  );
-}
