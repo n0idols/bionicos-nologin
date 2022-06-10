e022d17c67c0514448b600d1ca45383c5fa77fe9
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-06-10 08:20:54 -0700

    changed featured items

--- components/FeaturedProduct.js
+++ components/FeaturedProduct.js
@@ -6,10 +6,10 @@ import Loading from "./icons/Loading";
 
 export default function FeaturedProduct({ featuredProducts }) {
   return (
-    <div className="pt-10 max-w-5xl mx-auto px-2">
+    <div className="pt-10 max-w-3xl mx-auto px-2">
       {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
       <h1 className="grad-text">Featured Items</h1>
-      <div className="grid  md:grid-cols-3 grid-cols-1 gap-6 ">
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-6 ">
         {featuredProducts.products.map((item, index) => (
           <FeaturedItem key={index} item={item} />
         ))}
